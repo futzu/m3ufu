354e26c92cbb32a731ac3f10d8f123478ef3bd56
Adrian <52701496+futzu@users.noreply.github.com>
2023-08-23 00:02:30 +0000

    threefive 2.4.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     scripts=['bin/m3ufu'],
     install_requires=[
         "pyaes",
-        "threefive >= 2.3.75",
+        "threefive >= 2.4.1",
         "new_reader >= 0.1.7",
     ],
     classifiers=[
