25cc68b0d888f78f25903476826559b6f2b91cec
Adrian <52701496+futzu@users.noreply.github.com>
2023-09-11 02:18:01 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     scripts=['bin/m3ufu'],
     install_requires=[
         "pyaes",
-        "threefive >= 2.4.1",
+        "threefive >= 2.4.9",
         "new_reader >= 0.1.7",
     ],
     classifiers=[
